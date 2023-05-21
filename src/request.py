3f3d569b0618b42a983ca6b1a9b16af7ab7d10bb
Vladimir Kamenev <v.kamenev@g.nsu.ru>
2023-05-21 15:45:45 +0700

    request_resp_rewrite

--- src/request.py
+++ src/request.py
@@ -1,23 +1,22 @@
 from abc import ABC, abstractmethod
 
 
-class BaseRequestAPI(ABC):
+class BaseRequestInterface(ABC):
 
     @abstractmethod
     def to_json(self):
         raise NotImplementedError("Not Implemented")
 
 
-class MakeOrderRequest(BaseRequestAPI):
+class MakeOrderRequest(BaseRequestInterface):
     def __init__(self, user_id: int = 0, request_type: int = 0):
         self.user_id = user_id
-        self.request_type = request_type
 
     def to_json(self):
         pass
 
 
-class BookingRequest(BaseRequestAPI):
+class BookingRequest(BaseRequestInterface):
     def __init__(self, table_number: int = 0, number_of_guests: int = 0):
         self.table_number = table_number
         self.number_of_guests = number_of_guests
@@ -26,29 +25,65 @@ class BookingRequest(BaseRequestAPI):
         pass
 
 
-class RestaurantInfoRequest(BaseRequestAPI):
+class RestaurantInfoRequest(BaseRequestInterface):
     def __init__(self, user_id: int = 0, request_type: int = 0):
         self.user_id = user_id
-        self.request_type = request_type
 
     def to_json(self):
         pass
 
 
-class MenuRequest(BaseRequestAPI):
+class MenuRequest(BaseRequestInterface):
     def __init__(self, user_id: int = 0, request_type: int = 0):
         self.user_id = user_id
-        self.request_type = request_type
 
     def to_json(self):
         pass
 
 
-class FeedBackRequest(BaseRequestAPI):
+class FeedBackRequest(BaseRequestInterface):
     def __init__(self, user_id: int = 0, request_type: int = 0, feedback_text: str = 'Well done!'):
         self.user_id = user_id
-        self.request_type = request_type
         self.feedback_text = feedback_text
 
     def to_json(self):
         pass
+
+
+def order_req(*args):
+    return MakeOrderRequest(*args)
+
+
+def book_req(*args):
+    return BookingRequest(*args)
+
+
+def rest_info_req(*args):
+    return RestaurantInfoRequest(*args)
+
+
+def menu_req(*args):
+    return MenuRequest(*args)
+
+
+def feedback_req(*args):
+    return FeedBackRequest(*args)
+
+
+class RequestController:
+    def __init__(self, request_type: str = 'Menu'):
+        self.request = request_type
+
+    def set_params(self, *args):
+        if self.request == 'Order':
+            return order_req(*args)
+        elif self.request == 'Book':
+            return book_req(*args)
+        elif self.request == 'RestaurantInfo':
+            return rest_info_req(*args)
+        elif self.request == 'Menu':
+            return menu_req(*args)
+        elif self.request == 'Order':
+            return feedback_req(*args)
+
+
--- src/response.py
+++ src/response.py
@@ -1,9 +1,19 @@
-from abc import ABC,abstractmethod
-class BaseResponseAPI(ABC):
-    def __init__(self, user_id: int = 0, task: int = 0):
-        self.user_id = user_id
-        self.task = task
+from abc import ABC, abstractmethod
 
+
+class BaseResponseAPI(ABC):
     @abstractmethod
-    def to_json(self):
+    def from_json(self, json_file):
         raise NotImplementedError("Not Implemented")
+
+
+class ResponseController(BaseResponseAPI):
+    def __init__(self, json_file):
+        self.image, self.text = self.from_json(json_file)
+
+    def from_json(self, json_file):
+        """
+        KAK KARTINKU V JSONE PEREDAVAT?
+        :param json_file:
+        :return:
+        """
