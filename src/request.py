c5b63c023612a352e9bbc4b447055d2a50901166
Vladimir Kamenev <v.kamenev@g.nsu.ru>
2023-05-21 13:57:59 +0700

    added base classes

--- src/request.py
+++ src/request.py
@@ -2,9 +2,6 @@ from abc import ABC, abstractmethod
 
 
 class BaseRequestAPI(ABC):
-    def __init__(self, user_id: int = 0, task: int = 0):
-        self.user_id = user_id
-        self.task = task
 
     @abstractmethod
     def to_json(self):
@@ -12,25 +9,46 @@ class BaseRequestAPI(ABC):
 
 
 class MakeOrderRequest(BaseRequestAPI):
+    def __init__(self, user_id: int = 0, request_type: int = 0):
+        self.user_id = user_id
+        self.request_type = request_type
+
     def to_json(self):
         pass
 
 
 class BookingRequest(BaseRequestAPI):
+    def __init__(self, table_number: int = 0, number_of_guests: int = 0):
+        self.table_number = table_number
+        self.number_of_guests = number_of_guests
+
     def to_json(self):
         pass
 
 
 class RestaurantInfoRequest(BaseRequestAPI):
+    def __init__(self, user_id: int = 0, request_type: int = 0):
+        self.user_id = user_id
+        self.request_type = request_type
+
     def to_json(self):
         pass
 
 
 class MenuRequest(BaseRequestAPI):
+    def __init__(self, user_id: int = 0, request_type: int = 0):
+        self.user_id = user_id
+        self.request_type = request_type
+
     def to_json(self):
         pass
 
 
 class FeedBackRequest(BaseRequestAPI):
+    def __init__(self, user_id: int = 0, request_type: int = 0, feedback_text: str = 'Well done!'):
+        self.user_id = user_id
+        self.request_type = request_type
+        self.feedback_text = feedback_text
+
     def to_json(self):
         pass
